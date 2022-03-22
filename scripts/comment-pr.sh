2792e2a35e072b3d0c1b6d6d31ce1bd4b20f7d83
Justin Lei <justin.lei@grafana.com>
2022-03-22 12:29:19 -0700

    Fold code coverage comments

--- scripts/comment-pr.sh
+++ scripts/comment-pr.sh
@@ -11,21 +11,65 @@ then
   echo "Missing env var: DRONE_PULL_REQUEST"
   exit 1
 fi
+if [ -z "$DRONE_REPO_OWNER" ]
+then
+  echo "Missing env var: DRONE_REPO_OWNER"
+  exit 1
+fi
+if [ -z "$DRONE_REPO_NAME" ]
+then
+  echo "Missing env var: DRONE_REPO_NAME"
+  exit 1
+fi
 
 command -v jq >/dev/null 2>&1 || { echo "jq is not installed"; exit 1; }
 
-AUTH="grafanabot:${GRAFANABOT_PAT}"
-ACCEPT_HEADER="Accept: application/vnd.github.v3+json"
-ENDPOINT="https://api.github.com/repos/grafana/influx2cortex/issues/${DRONE_PULL_REQUEST}/comments"
-COMMENT_CONTENT=$1
+MINIMIZE_COMMENT=0
 
-if [ -z "$COMMENT_CONTENT" ]
+while getopts 'mc:' OPTION
+do
+  case $OPTION in
+    m) MINIMIZE_COMMENT=1
+      ;;
+    c) COMMENT="$OPTARG"
+      ;;
+    ?) echo "Unrecognized argument: $OPTION"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$COMMENT" ]
 then
   echo "Missing comment content; exiting early"
   exit 1
 fi
 
-BODY="{\"body\":$(echo "$COMMENT_CONTENT" | jq -aRs)}"
-echo "$BODY" > tmp-pr-comment.out
+AUTH_HEADER="Authorization: Bearer ${GRAFANABOT_PAT}"
+ENDPOINT="https://api.github.com/graphql"
 
-curl -u "$AUTH" -X POST -H "$ACCEPT_HEADER" "$ENDPOINT" -d @tmp-pr-comment.out
+github_graphql () {
+	PAYLOAD="{\"query\": $(echo $1 | jq -aRs)}"
+	JQ_PROCESSING=$2
+
+echo `curl -H "$AUTH_HEADER" -X POST -s -d "$PAYLOAD" "$ENDPOINT" | jq -r "$JQ_PROCESSING"`
+}
+
+# Get the node ID of the pull request
+
+PR_QUERY="query { repository(owner: \"$DRONE_REPO_OWNER\", name: \"$DRONE_REPO_NAME\") { pullRequest(number: $DRONE_PULL_REQUEST) { id } } }"
+PR_NODE_ID=`github_graphql "$PR_QUERY" '.data.repository.pullRequest.id'`
+
+# Add comment
+
+COMMENT_MUTATION="mutation { addComment(input: {subjectId: \"$PR_NODE_ID\", body: \"$COMMENT\"}) { commentEdge { node { id } } } }"
+COMMENT_NODE_ID=`github_graphql "$COMMENT_MUTATION" '.data.addComment.commentEdge.node.id'`
+
+# Minimize comment (optionally)
+if [ "$MINIMIZE_COMMENT" -eq 1 ]; then
+	MINIMIZE_MUTATION="mutation { minimizeComment(input: {subjectId: \"$COMMENT_NODE_ID\", classifier: RESOLVED}) { minimizedComment { isMinimized } } } "
+
+	github_graphql "$MINIMIZE_MUTATION" '.data.minimizeComment.minimizedComment.isMinimized' > /dev/null
+fi
+
+echo 'Done.'
