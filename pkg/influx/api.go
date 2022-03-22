177ab89da3369912ed51231e26d7d44d232aae38
Carrie Edwards <edwrdscarrie@gmail.com>
2022-03-22 16:26:45 -0500

    Move error handler into api.go

--- pkg/influx/api.go
+++ pkg/influx/api.go
@@ -1,6 +1,7 @@
 package influx
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
@@ -8,7 +9,9 @@ import (
 	"github.com/cortexproject/cortex/pkg/cortexpb"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
+	"github.com/grafana/influx2cortex/pkg/errorx"
 	"github.com/grafana/influx2cortex/pkg/remotewrite"
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/weaveworks/common/middleware"
@@ -51,8 +54,7 @@ func (a *API) handleSeriesPush(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := parseInfluxLineReader(r.Context(), r, maxSize)
 	if err != nil {
-		a.recorder.measureProxyErrors(fmt.Sprintf("%T", err))
-		handleError(w, r, a.logger, err)
+		a.handleError(w, r, err)
 		return
 	}
 	a.recorder.measureMetricsParsed(len(ts))
@@ -70,8 +72,7 @@ func (a *API) handleSeriesPush(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := a.client.Write(r.Context(), rwReq); err != nil {
-		a.recorder.measureProxyErrors(fmt.Sprintf("%T", err))
-		handleError(w, r, a.logger, err)
+		a.handleError(w, r, err)
 		return
 	}
 
@@ -79,4 +80,51 @@ func (a *API) handleSeriesPush(w http.ResponseWriter, r *http.Request) {
 	level.Debug(a.logger).Log("msg", "successful series write", "len", len(rwReq.Timeseries))
 
 	w.WriteHeader(http.StatusNoContent) // Needed for Telegraf, otherwise it tries to marshal JSON and considers the write a failure.
+}
+
+// handleError tries to extract an errorx.Error from the given error, logging
+// and setting the http response code as needed. All non-errorx errors are
+// considered internal errors. Please do not try to fix error categorization in
+// this function. All client errors should be categorized as an errorx at the
+// site where they are thrown.
+func (a *API) handleError(w http.ResponseWriter, r *http.Request, err error) {
+	var statusCode int
+	var httpErrString string
+	var errx errorx.Error
+	switch {
+	case errors.As(err, &errx):
+		httpErrString = errx.Message()
+		statusCode = errx.HTTPStatusCode()
+		err = errx
+	case errors.Is(err, context.DeadlineExceeded) || isGRPCTimeout(err):
+		httpErrString = "network timeout"
+		statusCode = http.StatusGatewayTimeout
+	case errors.Is(err, context.Canceled):
+		// Note: It seems unlikely this can happen other than as a timeout, so we
+		// should call it an internal error.
+		httpErrString = "request cancelled"
+		statusCode = http.StatusInternalServerError
+	case isNetworkTimeout(err):
+		if r.Body != nil {
+			// Try to read 1 byte from the request body. If it fails with the same error
+			// it means the timeout occurred while reading the request body, so it's a 408.
+			if _, readErr := r.Body.Read([]byte{0}); isNetworkTimeout(readErr) {
+				httpErrString = "response timeout"
+				statusCode = http.StatusRequestTimeout
+				break
+			}
+			httpErrString = "network timeout"
+			statusCode = http.StatusGatewayTimeout
+		}
+	default:
+		httpErrString = "uncategorized error"
+		statusCode = http.StatusInternalServerError
+	}
+	if statusCode < 500 {
+		level.Info(a.logger).Log("msg", httpErrString, "response_code", statusCode, "err", tryUnwrap(err))
+	} else if statusCode >= 500 {
+		level.Warn(a.logger).Log("msg", httpErrString, "response_code", statusCode, "err", tryUnwrap(err))
+	}
+	a.recorder.measureProxyErrors(fmt.Sprintf("%T", err))
+	http.Error(w, httpErrString, statusCode)
 }
--- pkg/influx/errors.go
+++ pkg/influx/errors.go
@@ -1,13 +1,8 @@
 package influx
 
 import (
-	"context"
 	"net"
-	"net/http"
 
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
-	"github.com/grafana/influx2cortex/pkg/errorx"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc/codes"
 
@@ -19,52 +14,6 @@ func tryUnwrap(err error) error {
 		return wrapped.Unwrap()
 	}
 	return err
-}
-
-// handleError tries to extract an errorx.Error from the given error, logging
-// and setting the http response code as needed. All non-errorx errors are
-// considered internal errors. Please do not try to fix error categorization in
-// this function. All client errors should be categorized as an errorx at the
-// site where they are thrown.
-func handleError(w http.ResponseWriter, r *http.Request, logger log.Logger, err error) {
-	var statusCode int
-	var httpErrString string
-	var errx errorx.Error
-	switch {
-	case errors.As(err, &errx):
-		httpErrString = errx.Message()
-		statusCode = errx.HTTPStatusCode()
-		err = errx
-	case errors.Is(err, context.DeadlineExceeded) || isGRPCTimeout(err):
-		httpErrString = "network timeout"
-		statusCode = http.StatusGatewayTimeout
-	case errors.Is(err, context.Canceled):
-		// Note: It seems unlikely this can happen other than as a timeout, so we
-		// should call it an internal error.
-		httpErrString = "request cancelled"
-		statusCode = http.StatusInternalServerError
-	case isNetworkTimeout(err):
-		if r.Body != nil {
-			// Try to read 1 byte from the request body. If it fails with the same error
-			// it means the timeout occurred while reading the request body, so it's a 408.
-			if _, readErr := r.Body.Read([]byte{0}); isNetworkTimeout(readErr) {
-				httpErrString = "response timeout"
-				statusCode = http.StatusRequestTimeout
-				break
-			}
-			httpErrString = "network timeout"
-			statusCode = http.StatusGatewayTimeout
-		}
-	default:
-		httpErrString = "uncategorized error"
-		statusCode = http.StatusInternalServerError
-	}
-	if statusCode < 500 {
-		level.Info(logger).Log("msg", httpErrString, "response_code", statusCode, "err", tryUnwrap(err))
-	} else if statusCode >= 500 {
-		level.Warn(logger).Log("msg", httpErrString, "response_code", statusCode, "err", tryUnwrap(err))
-	}
-	http.Error(w, httpErrString, statusCode)
 }
 
 func isNetworkTimeout(err error) bool {
