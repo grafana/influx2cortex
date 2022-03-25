974d77e04df348534fe6305fa28ca7dd34d40cb1
Justin Lei <justin.lei@grafana.com>
2022-03-25 12:48:32 -0700

    Fold the lint comment

--- scripts/generate-lint-report.sh
+++ scripts/generate-lint-report.sh
@@ -6,6 +6,9 @@
 command -v golangci-lint >/dev/null 2>&1 || { echo "golangci-lint is not installed"; exit 1; }
 
 echo 'Go lint report:' > lint.out
+echo '<details>' >> lint.out
+echo '<summary>Click to expand.</summary>' >> lint.out
 echo '```' >> lint.out
 golangci-lint run --issues-exit-code 0 >> lint.out
 echo '```' >> lint.out
+echo '</details>' >> lint.out
