e539ad5d1ed858047721e72793cc2a5000bcf5ec
Owen Williams <owen.williams@grafana.com>
2022-04-06 12:30:08 -0400

    even yet still more log lint

--- pkg/server/middleware/logging.go
+++ pkg/server/middleware/logging.go
@@ -9,8 +9,9 @@ import (
 	"github.com/uber/jaeger-client-go"
 
 	"github.com/go-kit/log"
-	"github.com/go-kit/log/level"
 	"github.com/weaveworks/common/user"
+
+	influxLog "github.com/grafana/influx2cortex/pkg/util/log"
 )
 
 type Log struct {
@@ -74,13 +75,13 @@ func logRequest(logger log.Logger, r *http.Request, statusCode int) {
 	if http.StatusContinue <= statusCode && statusCode < http.StatusInternalServerError ||
 		statusCode == http.StatusBadGateway ||
 		statusCode == http.StatusServiceUnavailable {
-		level.Info(logger).Log(
+		influxLog.Info(logger,
 			"method", r.Method,
 			"uri", redactAPIKey(r.URL).RequestURI(),
 			"status", statusCode,
 		)
 	} else {
-		level.Warn(logger).Log(
+		influxLog.Warn(logger,
 			"method", r.Method,
 			"uri", redactAPIKey(r.URL).RequestURI(),
 			"status", statusCode,
