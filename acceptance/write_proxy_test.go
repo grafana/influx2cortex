7f057f4e6d047fefa9224813498cbf1f88608776
Carrie Edwards <edwrdscarrie@gmail.com>
2022-04-12 12:50:06 -0700

    Update test response checking

--- acceptance/write_proxy_test.go
+++ acceptance/write_proxy_test.go
@@ -14,8 +14,8 @@ type Response struct {
 	Data   struct {
 		ResultType string `json:"resulttype"`
 		Result     []struct {
-			Metric model.Metric  `json:"metric"`
-			Value  []interface{} `json:"value"`
+			Metric model.Metric     `json:"metric"`
+			Sample model.SamplePair `json:"value"`
 		} `json:"result"`
 	} `json:"data"`
 }
@@ -23,27 +23,32 @@ type Response struct {
 func (s *Suite) Test_WriteLineProtocol() {
 	s.waitUntilElapsedAfterSuiteSetup(5 * time.Second)
 
-	line := fmt.Sprintf("stat,unit=temperature avg=%f", 23.5)
+	line := fmt.Sprintf("stat,unit=temperature,status=measured avg=%f", 23.5)
 	err := s.api.writeAPI.WriteRecord(context.Background(), line)
 	s.Require().NoError(err)
 
-	code, resp, err := s.api.proxy_client.query("prometheus/api/v1/query?query=stat_avg", "unknown")
+	expectedResult := model.Sample{
+		Metric: model.Metric{
+			model.MetricNameLabel:               "stat_avg",
+			model.LabelName("__proxy_source__"): "influx",
+			model.LabelName("unit"):             "temperature",
+			model.LabelName("status"):           "measured",
+		},
+		Value: 23.5,
+	}
+
+	s.verifyCortexWrite("prometheus/api/v1/query?query=stat_avg", expectedResult)
+}
+
+func (s *Suite) verifyCortexWrite(path string, expectedResult model.Sample) {
+	code, resp, err := s.api.proxy_client.query(path, "unknown")
 	s.Require().NoError(err)
 	s.Require().Equal(200, code)
-
-	expectedMetric := model.Metric{
-		model.MetricNameLabel:               "stat_avg",
-		model.LabelName("__proxy_source__"): "influx",
-		model.LabelName("unit"):             "temperature",
-	}
-	expectedVal := 23.5
 
 	var writeResponse Response
 	err = json.Unmarshal(resp, &writeResponse)
 	s.Require().NoError(err)
 
-	value := writeResponse.Data.Result[0].Value
-	metricVal := value[1]
-	s.Require().Equal(expectedMetric.Equal(writeResponse.Data.Result[0].Metric), true)
-	s.Require().Equal(metricVal, expectedVal)
+	s.Require().Equal(expectedResult.Metric.Equal(writeResponse.Data.Result[0].Metric), true)
+	s.Require().Equal(expectedResult.Value, writeResponse.Data.Result[0].Sample.Value)
 }
