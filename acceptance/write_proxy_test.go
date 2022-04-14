9865394d2b851d3684b07c37825c7361055124bc
Carrie Edwards <edwrdscarrie@gmail.com>
2022-04-14 07:52:52 -0700

    Clean up tests

--- acceptance/write_proxy_test.go
+++ acceptance/write_proxy_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/prometheus/common/model"
 )
 
-func (s *Suite) Test_WriteLineProtocol() {
+func (s *Suite) Test_WriteLineProtocol_SingleMetric() {
 	line := fmt.Sprintf("stat,unit=temperature,status=measured avg=%f", 23.5)
 	err := s.api.writeAPI.WriteRecord(context.Background(), line)
 	s.Require().NoError(err)
@@ -27,11 +27,11 @@ func (s *Suite) Test_WriteLineProtocol() {
 	result, _, err := s.api.querierClient.Query(context.Background(), "stat_avg", time.Now())
 	s.Require().NoError(err)
 
-	vector := result.(model.Vector)
-	s.Require().Len(vector, 1)
+	resultVector := result.(model.Vector)
+	s.Require().Len(resultVector, 1)
 
-	s.Require().Equal(expectedResult.Metric, vector[0].Metric)
-	s.Require().Equal(expectedResult.Value, vector[0].Value)
+	s.Require().Equal(expectedResult.Metric, resultVector[0].Metric)
+	s.Require().Equal(expectedResult.Value, resultVector[0].Value)
 }
 
 func (s *Suite) Test_WriteLineProtocol_MultipleFields() {
@@ -57,17 +57,17 @@ func (s *Suite) Test_WriteLineProtocol_MultipleFields() {
 
 	result, _, err := s.api.querierClient.Query(context.Background(), "{__name__=~\"measurement_.+\",__proxy_source__=\"influx\"}", time.Now())
 	s.Require().NoError(err)
-	vector := result.(model.Vector)
-	s.Require().Len(vector, 2)
+	resultVector := result.(model.Vector)
+	s.Require().Len(resultVector, 2)
 
-	s.Require().Equal(expectedResult1.Metric, vector[0].Metric)
-	s.Require().Equal(expectedResult1.Value, vector[0].Value)
-	s.Require().Equal(expectedResult2.Metric, vector[1].Metric)
-	s.Require().Equal(expectedResult2.Value, vector[1].Value)
+	s.Require().Equal(expectedResult1.Metric, resultVector[0].Metric)
+	s.Require().Equal(expectedResult1.Value, resultVector[0].Value)
+	s.Require().Equal(expectedResult2.Metric, resultVector[1].Metric)
+	s.Require().Equal(expectedResult2.Value, resultVector[1].Value)
 
 }
 
-func (s *Suite) Test_WriteLineProtocol_MultipleSeries() {
+func (s *Suite) Test_WriteLineProtocol_MetricWithDifferentTags() {
 	var err error
 	lines := []string{
 		"sample,tag1=val1 metric=3",
@@ -107,18 +107,18 @@ func (s *Suite) Test_WriteLineProtocol_MultipleSeries() {
 	result, _, err := s.api.querierClient.Query(context.Background(), "sample_metric", time.Now())
 	s.Require().NoError(err)
 
-	vector := result.(model.Vector)
-	s.Require().Len(vector, 3)
+	resultVector := result.(model.Vector)
+	s.Require().Len(resultVector, 3)
 
-	s.Require().Equal(expectedResult1.Metric, vector[0].Metric)
-	s.Require().Equal(expectedResult1.Value, vector[0].Value)
-	s.Require().Equal(expectedResult2.Metric, vector[1].Metric)
-	s.Require().Equal(expectedResult2.Value, vector[1].Value)
-	s.Require().Equal(expectedResult3.Metric, vector[2].Metric)
-	s.Require().Equal(expectedResult3.Value, vector[2].Value)
+	s.Require().Equal(expectedResult1.Metric, resultVector[0].Metric)
+	s.Require().Equal(expectedResult1.Value, resultVector[0].Value)
+	s.Require().Equal(expectedResult2.Metric, resultVector[1].Metric)
+	s.Require().Equal(expectedResult2.Value, resultVector[1].Value)
+	s.Require().Equal(expectedResult3.Metric, resultVector[2].Metric)
+	s.Require().Equal(expectedResult3.Value, resultVector[2].Value)
 }
 
-func (s *Suite) Test_WriteLineProtocol_MultiplePoints() {
+func (s *Suite) Test_WriteLineProtocol_MultipleMetrics() {
 	lines := []string{
 		"test_metric,test=1,tag=2 foo=1",
 		"test_metric_time,test=1,tag=4 sample=3.14",
@@ -165,13 +165,13 @@ func (s *Suite) Test_WriteLineProtocol_MultiplePoints() {
 		})
 	s.Require().NoError(err)
 
-	matrix := result.(model.Matrix)
-	s.Require().Len(matrix, 3)
+	resultMatrix := result.(model.Matrix)
+	s.Require().Len(resultMatrix, 3)
 
-	s.Require().Equal(expectedResult1.Metric, matrix[0].Metric)
-	s.Require().Equal(expectedResult1.Value, matrix[0].Values[0].Value)
-	s.Require().Equal(expectedResult2.Metric, matrix[1].Metric)
-	s.Require().Equal(expectedResult2.Value, matrix[1].Values[0].Value)
-	s.Require().Equal(expectedResult3.Metric, matrix[2].Metric)
-	s.Require().Equal(expectedResult3.Value, matrix[2].Values[0].Value)
+	s.Require().Equal(expectedResult1.Metric, resultMatrix[0].Metric)
+	s.Require().Equal(expectedResult1.Value, resultMatrix[0].Values[0].Value)
+	s.Require().Equal(expectedResult2.Metric, resultMatrix[1].Metric)
+	s.Require().Equal(expectedResult2.Value, resultMatrix[1].Values[0].Value)
+	s.Require().Equal(expectedResult3.Metric, resultMatrix[2].Metric)
+	s.Require().Equal(expectedResult3.Value, resultMatrix[2].Values[0].Value)
 }
