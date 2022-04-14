437d8d977163911de4211d7a4607679ffac2e7ea
Carrie Edwards <edwrdscarrie@gmail.com>
2022-04-14 09:47:50 -0700

    Update comment

--- pkg/influx/api.go
+++ pkg/influx/api.go
@@ -20,7 +20,7 @@ type API struct {
 func (a *API) Register(router *mux.Router) {
 	registerer := route.NewMuxRegisterer(router)
 
-	// Registering two write endpoints; the second is necessary to allow for direct pushes to the proxy
+	// Registering two write endpoints; the second is necessary to allow for compatibility with clients that hard-code the endpoint
 	registerer.RegisterRoute("/api/v1/push/influx/write", http.HandlerFunc(a.handleSeriesPush), http.MethodPost)
 	registerer.RegisterRoute("/api/v2/write", http.HandlerFunc(a.handleSeriesPush), http.MethodPost)
 }
