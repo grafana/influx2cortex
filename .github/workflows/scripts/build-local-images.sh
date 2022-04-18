4a9b93959b03dd7d7bf8b2edba9c6d46404acdca
Carrie Edwards <edwrdscarrie@gmail.com>
2022-04-18 13:52:15 -0700

    try full path

--- .github/workflows/scripts/build-local-images.sh
+++ .github/workflows/scripts/build-local-images.sh
@@ -7,10 +7,6 @@ IFS=$'\t\n'
 echo "Test"
 command -v docker >/dev/null 2>&1 || { echo 'Please install docker'; exit 1; }
 
-echo "# Compiling go binaries"
-bash ./scripts/compile-commands.sh
-echo "done"
-
 echo "# Building docker images"
 # If this gets too slow, we should allow users to only build the images for one proxy
 for cmd in influx2cortex
