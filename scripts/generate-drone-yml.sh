bc1e61b88c921c5483557c50a760dfc4f3769dc9
Carrie Edwards <edwrdscarrie@gmail.com>
2022-04-19 09:09:19 -0700

    Updated drone lint to trusted

--- scripts/generate-drone-yml.sh
+++ scripts/generate-drone-yml.sh
@@ -2,5 +2,5 @@
 set -eufo pipefail
 
 drone jsonnet --source .drone/drone.jsonnet --target .drone/drone.yml --stream --format
-drone lint .drone/drone.yml
+drone lint --trusted .drone/drone.yml
 drone sign --save grafana/influx2cortex .drone/drone.yml
