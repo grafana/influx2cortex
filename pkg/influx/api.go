e36de4799d76c01add4e839f5129aa3c50a5ed78
Nicolás Pazos <npazosmendez@gmail.com>
2024-10-02 10:09:27 -0300

    instrumentation: add `handleSeriesPush` span

--- pkg/influx/api.go
+++ pkg/influx/api.go
@@ -12,6 +12,8 @@ import (
 	"github.com/grafana/mimir-proxies/pkg/route"
 	"github.com/grafana/mimir-proxies/pkg/server/middleware"
 	"github.com/grafana/mimir/pkg/mimirpb"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 )
 
 type API struct {
@@ -40,29 +42,24 @@ func NewAPI(conf ProxyConfig, client remotewrite.Client, recorder Recorder) (*AP
 
 // HandlerForInfluxLine is a http.Handler which accepts Influx Line protocol and converts it to WriteRequests.
 func (a *API) handleSeriesPush(w http.ResponseWriter, r *http.Request) {
-	logger := a.logger
-	if traceID, ok := middleware.ExtractSampledTraceID(r.Context()); ok {
-		logger = log.With(logger, "traceID", traceID)
-	}
-	if orgID, err := user.ExtractOrgID(r.Context()); err == nil {
-		logger = log.With(logger, "orgID", orgID)
-	}
-	if userID, err := user.ExtractUserID(r.Context()); err == nil {
-		logger = log.With(logger, "userID", userID)
-	}
-	logger = log.With(logger, "path", r.URL.EscapedPath())
+	span, ctx := opentracing.StartSpanFromContext(r.Context(), "handleSeriesPush")
+	defer span.Finish()
 
+	logger := withRequestInfo(a.logger, r)
 	beforeConversion := time.Now()
 
-	ts, err, bytesRead := parseInfluxLineReader(r.Context(), r, a.maxRequestSizeBytes)
+	ts, err, bytesRead := parseInfluxLineReader(ctx, r, a.maxRequestSizeBytes)
+	span.LogKV("bytesRead", bytesRead)
 	logger = log.With(logger, "bytesRead", bytesRead)
 	if err != nil {
+		ext.LogError(span, err)
 		a.handleError(w, r, err, logger)
 		return
 	}
 
 	nosMetrics := len(ts)
 	logger = log.With(logger, "nosMetrics", nosMetrics)
+	span.LogKV("nosMetrics", nosMetrics)
 
 	a.recorder.measureMetricsParsed(nosMetrics)
 	a.recorder.measureConversionDuration(time.Since(beforeConversion))
@@ -78,12 +75,28 @@ func (a *API) handleSeriesPush(w http.ResponseWriter, r *http.Request) {
 		Timeseries: pts,
 	}
 
-	if err := a.client.Write(r.Context(), rwReq); err != nil {
+	if err := a.client.Write(ctx, rwReq); err != nil {
+		ext.LogError(span, err)
 		a.handleError(w, r, err, logger)
 		return
 	}
 	a.recorder.measureMetricsWritten(len(rwReq.Timeseries))
+	span.LogKV("nosMetricsWritten", len(rwReq.Timeseries))
 	statusCode := http.StatusNoContent
 	_ = level.Info(logger).Log("response_code", statusCode)
 	w.WriteHeader(statusCode) // Needed for Telegraf, otherwise it tries to marshal JSON and considers the write a failure.
+}
+
+func withRequestInfo(logger log.Logger, r *http.Request) log.Logger {
+	ctx := r.Context()
+	if traceID, ok := middleware.ExtractSampledTraceID(ctx); ok {
+		logger = log.With(logger, "traceID", traceID)
+	}
+	if orgID, err := user.ExtractOrgID(ctx); err == nil {
+		logger = log.With(logger, "orgID", orgID)
+	}
+	if userID, err := user.ExtractUserID(ctx); err == nil {
+		logger = log.With(logger, "userID", userID)
+	}
+	return log.With(logger, "path", r.URL.EscapedPath())
 }
